e8a2e68a224402c74de82bd509d7376a0536735b
Anderson Juhasc <anderson.juhasc@gmail.com>
2026-01-02 13:15:43 -0300

    feat: add app quit keyboard shortcuts

--- composeApp/src/jvmMain/kotlin/org/nostr/nostrord/main.kt
+++ composeApp/src/jvmMain/kotlin/org/nostr/nostrord/main.kt
@@ -1,5 +1,11 @@
 package org.nostr.nostrord
 
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.isMetaPressed
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -7,6 +13,19 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Nostrord",
+        onPreviewKeyEvent = { event ->
+            if (event.type == KeyEventType.KeyDown) {
+                // Ctrl+Q (Windows/Linux) or Cmd+Q (macOS) - quit application
+                if (event.key == Key.Q && (event.isCtrlPressed || event.isMetaPressed)) {
+                    exitApplication()
+                    true
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        }
     ) {
         App()
     }
